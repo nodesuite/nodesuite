b06c22a2134478bd4409597572b0cfaffba99c56
Prescience <65471523+prescience-data@users.noreply.github.com>
2022-08-09 13:39:10 +1000

    feat: initial commit

---
+++ .ncurc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "upgrade": true,
+  "reject": [
+    "globby"
+  ],
+  "loglevel": "error",
+  "timeout": 1000 * 60,
+  "errorLevel": 1
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  endOfLine: "lf",
+  singleQuote: false,
+  bracketSpacing: true,
+  printWidth: 80,
+  trailingComma: "none",
+  arrowParens: "always",
+  tabWidth: 2,
+}
