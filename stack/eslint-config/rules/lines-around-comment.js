113ec10cb866fb673cc68eb3f3f85d0ccc4bdbec
Prescience <65471523+prescience-data@users.noreply.github.com>
2022-12-28 08:49:24 +1100

    feat: allow inline array comments
    enables new eslint feature to allow comments at start of array

--- stack/eslint-config/rules/lines-around-comment.js
+++ stack/eslint-config/rules/lines-around-comment.js
@@ -5,6 +5,11 @@ module.exports = {
   beforeLineComment: true,
   afterLineComment: false,
   allowBlockStart: true,
+  allowBlockEnd: false,
   allowClassStart: true,
-  allowObjectStart: true
+  allowClassEnd: false,
+  allowObjectStart: true,
+  allowObjectEnd: false,
+  allowArrayStart: true,
+  allowArrayEnd: false
 }
