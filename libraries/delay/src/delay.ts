4aa3171a54324140b77122a256051f97b1a86d46
Prescience <65471523+prescience-data@users.noreply.github.com>
2022-12-28 09:06:10 +1100

    feat: switch to node 15 timer promise

--- libraries/delay/src/delay.ts
+++ libraries/delay/src/delay.ts
@@ -1,24 +1,24 @@
 import { randomInt } from "node:crypto"
+import { setTimeout } from "node:timers/promises"
 
 import type { Delay } from "./types"
 
 /**
  * Pauses execution for a specific or randomized duration.
  *
+ * @remarks
+ * Uses Node built-in timer promise which requires Node v15+.
+ *
  * @public
  */
 export const delay: Delay = async (min: number, max?: number): Promise<void> =>
-  new Promise((resolve) => {
-    setTimeout(
-      () => {
-        resolve()
-      },
-      max ? randomInt(min, max) : min
-    )
-  })
+  await setTimeout(max ? randomInt(min, max) : min)
 
 /**
  * Pauses execution for a specific or randomized duration.
+ *
+ * @remarks
+ * Alias of `delay()`.
  *
  * @public
  */
@@ -26,6 +26,9 @@ export const sleep: Delay = delay
 
 /**
  * Pauses execution for a specific or randomized duration.
+ *
+ * @remarks
+ * Alias of `delay()`.
  *
  * @public
  */
