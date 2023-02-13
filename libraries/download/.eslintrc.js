f2fe9fcf006edbce161d172864e0182dcb95d79e
Prescience <65471523+prescience-data@users.noreply.github.com>
2023-02-13 11:08:34 +1100

    feat: remove unused download library

--- libraries/download/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-require("@rushstack/eslint-config/patch/modern-module-resolution")
-
-module.exports = {
-  extends: [
-    "@rushstack/eslint-config/profile/node",
-    "@rushstack/eslint-config/mixins/friendly-locals",
-    "@nodesuite/eslint-config"
-  ]
-}
